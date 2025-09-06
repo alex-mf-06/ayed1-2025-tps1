eb79d0470168dee61e47978e91a289dc5221180c
alex-mf-06 <mamanifloresalexander8@gmail.com>
2025-09-06 20:47:12 -0300

    ejercicios 5,6,7,8,9 ya resueltos

--- tp2/TP2.py
+++ tp2/TP2.py
@@ -116,4 +116,87 @@ def carg_lista():
     print(F"{lista}")
     print(f"Los ultimos 10 elementos son : {lista[-10:]}")
 carg_lista()
+#ejercicio 4
+def eliminar_valor_lista(lista: list[int], lista_eliminar: list[int]) -> list[int]:
+    for i in lista_eliminar: # Itera sobre los elementos a eliminar
+        while i in lista:    # Se repite hasta eliminar todas las ocurrencias
+            lista.remove(i)
+    return lista # Lista final sin los valores eliminados
+lista_a = [1,1,2,3,4,4,5,6,78,21]
+lista_con_valores_a_eliminar = [1, 4, 9, 20, 21]
+print(f"lista original : {lista_a}")
+print(guion(70))
+print(f"lista con valores a eliminar : {lista_con_valores_a_eliminar}")
+print(guion(70))
+print(f"lista final : {eliminar_valor_lista(lista_a, lista_con_valores_a_eliminar)}")
+#ejercicio 5
+def ordenar_(lista:list[int]) -> bool : 
+    """
+    la funcion va a retornar True si la lista esta ordenada de manera ascendente y False en caso contrario
+    siempre y cuando la lista no este vacia y este ordenada. 
+    contrato : la lista no debe estar vacia y debe estar ordenada
+    precondiciones : la lista no debe estar vacia
+    postcondiciones : la lista debe estar ordenada
+    """
+    return lista == sorted(lista)
 
+
+print(ordenar_([1,2,3,4,5,6,7,8,9])) # True
+print(ordenar_([9,8,7,6,5,4,3,2,1])) # False
+assert ordenar_([1,2,3,4,5,6,7,8,9]) == True
+assert ordenar_([9,8,7,6,5,4,3,2,1]) == False
+assert ordenar_(["a","b","c"]) == True
+assert ordenar_(["c","b","a"]) == False
+
+#ejercicio 6 
+def normalizar(lista: list[int]) -> list[float]:
+    """
+    Normaliza los valores de una lista de números enteros a una escala de 0 a 1.
+    Contrato: La lista no debe estar vacía.
+
+    """
+    if not lista:
+        raise ValueError("La lista no debe estar vacía.")
+    return [x / sum(lista) for x in lista] # Normaliza los valores a una escala de 0 a 1
+print(normalizar([1,2,2]))
+assert normalizar([1,2,2]) == [0.2, 0.4, 0.4] # Normaliza a [0.2, 0.4, 0.4]
+assert normalizar([9,9,9,13]) == [0.225, 0.225, 0.225, 0.325] #aproximado
+
+#ejercicio 7 
+def intercalando(lista1: list[int], lista2: list[int]) -> list[int]:
+    """
+    Intercala dos listas de números enteros y las vuelve en una nueva lista(se añade a la lista 1 -__-) con todos los elementos de las 2 anteriores listas.
+
+    """
+    for i,v in enumerate(lista2): # Itera sobre los elementos de la segunda lista
+        indice = i * 2 + 1 # Calcula el índice de inserción
+        lista1[indice:indice] = [v] # Inserta el elemento de la segunda lista en la posición calculada
+    return lista1
+
+print(intercalando([1,3,5],[2,4,6])) # [1,2,3,4,5,6]
+
+assert intercalando([1,3,5],[2,4,6]) == [1,2,3,4,5,6]
+assert intercalando([10,20,30],[15,25,35]) == [10,15,20,25,30,35]
+assert intercalando([1,2,3],[4,5,6]) != [1,4,2,5,3,2]
+
+#ejercicio 8    
+def impares(inicio:int, fin:int) -> list[int]:
+    """
+    Genera una lista de números impares en un rango dado entre inicio y fin.
+    """
+    return [i for i in range(inicio, fin + 1) if i % 2 != 0]
+
+impares_lista = impares(100, 200)
+print(impares_lista)  # Muestra la lista de números impares entre 100 y 200
+def multiplo_7_no_de_5(inicio:int , fin:int)-> list[int]:
+    """
+    Genera una lista de números que son múltiplos de 7 pero no de 5 en un rango dado
+    """
+    return [i for i in range(inicio, fin + 1) if i % 7 == 0 and i % 5 != 0]
+print(multiplo_7_no_de_5(1, 100))
+#ejercicio 9
+cantidad = 20
+lista_random = [random.randint(1,100) for x in range(cantidad)]
+impares = list(filter(lambda x : x % 2 != 0 , lista_random))
+print(impares)
+#ejercicio 10
